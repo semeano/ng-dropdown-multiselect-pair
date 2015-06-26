08fbdb51b595b5cfefc42ecc600924069cd9d40d
Pedro Semeano <semeano@gmail.com>
2015-06-26 12:39:29 +0100

    Button to add new item.

--- src/ng-dropdown-multiselect-pair.js
+++ src/ng-dropdown-multiselect-pair.js
@@ -34,7 +34,7 @@
 	            template += '<li ng-show="settings.enableSearch && !settings.noSeparators" class="divider"></li>';
 
 	            // New item
-	            template += '<li ng-show="settings.enableNewItem"><div class="dropdown-header"><div class="dropdown-header-new-item-label"><input type="text" class="form-control" ng-model="newItemLabel" placeholder="{{texts.newItemPlaceholder[0]}}" ng-keydown="onNewItemAddKeyDown($event)" /></div><div class="dropdown-header-new-item-extra"><input type="text" class="form-control" ng-model="newItemExtra" placeholder="{{texts.newItemPlaceholder[1]}}" ng-keydown="onNewItemAddKeyDown($event)" /></div></div></li>';
+	            template += '<li ng-show="settings.enableNewItem"><div class="dropdown-header"><div class="dropdown-header-new-item-label"><input type="text" class="form-control" ng-model="newItemLabel" placeholder="{{texts.newItemPlaceholder[0]}}" ng-keydown="onNewItemAddKeyDown($event)" /></div><div class="dropdown-header-new-item-extra"><input type="text" class="form-control" ng-model="newItemExtra" placeholder="{{texts.newItemPlaceholder[1]}}" ng-keydown="onNewItemAddKeyDown($event)" /></div><span class="glyphicon glyphicon-plus icon-plus" ng-click="onNewItemAddClick()"></span></div></li>';
 	            template += '<li ng-show="settings.enableNewItem && !settings.noSeparators" class="divider"></li>';
 
           if (groups) {
@@ -382,6 +382,11 @@
           		scope.newItemExtra = '';
           		event.preventDefault();
           	}
+          };
+          scope.onNewItemAddClick = function () {
+        		scope.events.onNewItemAdd(scope.newItemLabel, scope.newItemExtra);
+        		scope.newItemLabel = '';
+        		scope.newItemExtra = '';
           };
 
           scope.externalEvents.onInitDone();
